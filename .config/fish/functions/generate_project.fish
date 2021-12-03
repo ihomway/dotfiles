1785257183ae89ee554e17789d0eb96369705df6
puer <ihomway@live.com>
2021-12-03 17:59:41 +0800

    Add generate_project function

---
+++ .config/fish/functions/generate_project.fish
@@ -0,0 +1,3 @@
+function generate_project --description 'Generate Xcode Project by Tusil'
+command ./tools/Tulsi.app/Contents/MacOS/Tulsi -- --genconfig "bili-universal/bilianime.tulsi.tulsiproj:bili-universal"
+end
