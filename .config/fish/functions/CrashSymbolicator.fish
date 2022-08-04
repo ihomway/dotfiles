ca58b360dddfb01714294083e43299a5e33da4b8
puer <ihomway@live.com>
2022-08-04 16:34:59 +0800

    Add CrashSymbolicator

---
+++ .config/fish/functions/CrashSymbolicator.fish
@@ -0,0 +1,3 @@
+function CrashSymbolicator --description 'Xcode CrashSymbolicator'
+python /Applications/Xcode.app/Contents/SharedFrameworks/CoreSymbolicationDT.framework/Resources/CrashSymbolicator.py $argv
+end
