c54f021ad1ce8dcceeb2c573ee2e478642716159
Puer <ihomway@live.ocm>
2023-07-20 16:13:42 +0800

    replace bgba.fish bgst.fish by aabr

--- .config/fish/functions/bgba.fish
+++
@@ -1,7 +0,0 @@
-function bgba --description './bbgit branch -a -v'
-    if test -e bbgit
-        command ./bbgit branch -a -v
-    else 
-        command gba
-    end
-end
--- .config/fish/functions/bgst.fish
+++
@@ -1,7 +0,0 @@
-function bgst --description './bbgit status'
-    if test -e bbgit
-        command ./bbgit status
-    else 
-        command gst
-    end
-end
