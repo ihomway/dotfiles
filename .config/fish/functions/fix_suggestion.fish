e1801fbd6a96e3fe019a1bfe106bc042de857722
puer <ihomway@live.com>
2023-11-29 15:42:22 +0800

    Remove unused functions

--- .config/fish/functions/fix_suggestion.fish
+++
@@ -1,5 +0,0 @@
-function fix_suggestion --description 'Fix Project Suggestion'
-	echo 'Start Replacing...'
-	command sed -i.bak "s/x86_64-/arm64-/g" bili-universal/bilianime.xcodeproj/project.pbxproj
-	echo 'Replacing Finished!'
-end
